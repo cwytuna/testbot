1e21c2f9833ced4b35cd461c8e505edce4bbe5f6
cwytuna <117967021+cwytuna@users.noreply.github.com>
2022-11-11 15:36:33 +0800

    Update app_core.py

--- app_core.py
+++ app_core.py
@@ -4,11 +4,13 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('T0HwEDTign1i80yWEhT66wBk5jqqGhYLeShPY2+TEmgcBguKGk1YsgNHXgJKmz4huW1Q0WjENef7BbrmHHoKs6tddEsyM/uPMURRrIvpHd+z3t2mbjDA45xC6niuLyV53JEqGG4Kv+mA0Kt+70KMegdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('127293839562d33b530ba756e7baf03b')
+line_bot_api = LineBotApi('聊天機器人的 Chennel access token')
+handler = WebhookHandler('聊天機器人的 Channel secret')
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -24,6 +26,16 @@ def callback():
         abort(400)
 
     return 'OK'
+
+# 學你說話
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 
 if __name__ == "__main__":
     app.run()
